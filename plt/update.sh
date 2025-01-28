233fbbee48983a77fad6a7dcdbea5124b2e80915
Joey Eremondi <joey@eremondi.com>
2025-01-28 12:20:35 -0600

    Update PLT file

--- plt/update.sh
+++ plt/update.sh
@@ -3,10 +3,12 @@
 cd ~/350/plt
 
 # print the current timestamp to the file
-date +"20%y%m%d%H%M" >~/Library/Racket/8.11.1/collects/cs350-handin-client/version
+date +"20%y%m%d%H%M" >~/Library/Racket/8.11.1/pkgs/handin/handin-client/version
 
 # copy the file for the server
 cp ~/Library/Racket/8.11.1/pkgs/handin/handin-client/version ./
+
+cat ./version
 
 raco pack --collect --at-plt --replace ++setup cs350-handin-client cs350.plt cs350-handin-client flit todo-list websocket
 
