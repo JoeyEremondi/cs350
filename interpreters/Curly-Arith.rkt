36b9b92164575a6d4f779558ea9e946bae4aacb1
Joey Eremondi <joey@eremondi.com>
2025-02-21 16:35:36 -0600

    Update fold notes

--- interpreters/Curly-Arith.rkt
+++ interpreters/Curly-Arith.rkt
@@ -1,4 +1,4 @@
-#lang plait
+#lang flit
 
 ;; Curly-Arith: Creating our first programming language
 ;; A simple language with numbers, addition and multiplication
@@ -14,49 +14,65 @@
 
 ;; Abstract syntax for Curly-Arith
 ;; Represents expressions in our interpreter
-(define-type Expr
+(define-type Exp
   ;; Constant numbers
-  (NumLit [n : Number])
+  (num [n : Number])
   ;; {+ e1 e2}
-  (Plus [left : Expr]
-        [right : Expr])
+  (plus [left : Exp]
+        [right : Exp])
   ;; {* e1 e2}
-  (Times [left : Expr]
-         [right : Expr]))
+  (times [left : Exp]
+         [right : Exp]))
+
+;; Value is the type that our interpreter produces
+;; We don't need to define a datatype, we can just use Number.
+;; We define a type alias so that we can swap Number out for
+;; more interesting types later on.
+(define-type-alias Value Number)
 
 ;; Parse
 ;; Takes an S-expression and turns it into an Expr
-;; Raises an error if 
-(define (parse [s : S-Exp]) : Expr
+;; Raises an error if it's not a valid Curly-Arith program
+;;
+;; parse : S-Exp -> Exp
+(define (parse [s : S-Exp])
+  : Exp
   (cond
     ;; Constant number e.g. 5
-    [(s-exp-match? `NUMBER s) (NumLit (s-exp->number s))]
+    [(s-exp-match? `NUMBER s) (num (s-exp->number s))]
     ;; {+ s1 s2}
     [(s-exp-match? `{+ ANY ANY} s)
-     (Plus (parse (second (s-exp->list s)))
+     (plus (parse (second (s-exp->list s)))
            (parse (third (s-exp->list s))))]
     ;; {* s1 s2}
     [(s-exp-match? `{* ANY ANY} s)
-     (Times (parse (second (s-exp->list s)))
+     (times (parse (second (s-exp->list s)))
             (parse (third (s-exp->list s))))]
     [else (error 'parse "invalid input")]))
 
+
+
 ;; Evaluate Expressions
-(define (interp [e : Expr] ) : Number
-  (type-case Expr e
+;; The texbook calls this cal for arithmetic
+;;
+;; interp : Exp -> Value
+(define (interp [e : Exp] ) : Value
+  (type-case Exp e
     ;; A number evaluates to itself
-    [(NumLit n) n]
+    [(num n) n]
     ;; {+ e1 e2} evaluates e1 and e2, then adds the results together
-    [(Plus l r)
+    [(plus l r)
      (+ (interp l) (interp r))]
     ;; Works the same but for times
-    [(Times l r)
+    [(times l r)
      (* (interp l) (interp r))]))
 
 ;; The Language Pipeline
 ;; We run  program by parsing an s-expression
 ;; Implicit: we can turn strings into s-expressions using Racket's quote
 ;; i.e. `{+ 3 4} generates an S-expression directly
+;;
+;; run : S-Exp -> Number
 (define (run s-exp) (interp (parse s-exp)))
 
 (test (run `3) 3)
